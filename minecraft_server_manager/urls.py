d2ed693ca403191d507cb0b32807b05ca19c3d3a
SecretJuJu <dev.tmdqh@gmail.com>
2022-05-23 23:03:22 +0900

    feat(app/minecraft_server-manager): add redirect default link to /server_manger

--- minecraft_server_manager/urls.py
+++ minecraft_server_manager/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('minecraft_server_manager.apps.user.urls')),
-    path('', include('minecraft_server_manager.apps.server_manager.urls')),
+    path('', RedirectView.as_view(url='/server_manager/')),
+    path('server_manager/', include('minecraft_server_manager.apps.server_manager.urls'))
 ]
