f5d5898553bb7d569a29844fdd4a58c827583135
SecretJuJu <dev.tmdqh@gmail.com>
2022-05-23 23:18:30 +0900

    chore(app/minecraft_server-manager): login 이 필요한 서비스를 @login_require decortor로 관리

--- minecraft_server_manager/apps/server_manager/views.py
+++ minecraft_server_manager/apps/server_manager/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request=request, template_name='server_manager/list.html')
 
 
+@login_required
 def create(request):
     if request.method == 'GET':
         return render(request=request, template_name='server_manager/create.html')
