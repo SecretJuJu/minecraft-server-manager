3337c75e114373874338d715aa59f975b12d86ee
SecretJuJu <dev.tmdqh@gmail.com>
2022-05-23 23:12:37 +0900

    feat(app/minecraft_server-manager): add route to create server_manager

--- minecraft_server_manager/apps/server_manager/views.py
+++ minecraft_server_manager/apps/server_manager/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request=request, template_name='server_manager/list.html')
+
+
+def create(request):
+    if request.method == 'GET':
+        return render(request=request, template_name='server_manager/create.html')
+
+    if request.method == 'POST':
+        return HttpResponse('you are welcome')
+
+    return HttpResponse.status_code(404)
+
