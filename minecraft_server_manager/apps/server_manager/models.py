ad2c626b43da083ce93ac55e414da4d28d089e52
SecretJuJu <dev.tmdqh@gmail.com>
2022-05-25 00:38:07 +0900

    feat(apps/server_manager): add is_server_running field in ServerManager model

--- minecraft_server_manager/apps/server_manager/models.py
+++ minecraft_server_manager/apps/server_manager/models.py
@@ -7,6 +7,7 @@ class ServerManager(models.Model):
     title = models.CharField(max_length=40)
     version = models.CharField(max_length=10)
     map_zip_file = models.FileField(upload_to="uploads/map/origins", blank=True)
+    is_server_running = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
